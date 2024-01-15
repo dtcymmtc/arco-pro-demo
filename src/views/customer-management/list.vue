a6c1ba7d30dd73d3770eea14d25a1ab12cc5a518
dtcymmtc <dtcymmtc@163.com>
2024-01-15 23:49:18 +0800

    优化布局

--- src/views/customer-management/list.vue
+++ src/views/customer-management/list.vue
@@ -1,3 +1,61 @@
 <template>
-  <div>客户查询</div>
+  <div class="container">
+    <Breadcrumb
+      :items="['menu.customerManagement', 'menu.customerManagement.list']"
+    />
+    <a-row :gutter="20" align="stretch">
+      <a-col :span="24">
+        <a-card
+          class="general-card"
+          :title="$t('menu.customerManagement.list')"
+        >
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
 </template>
+
+<style scoped lang="less">
+  .container {
+    padding: 0 20px 20px 20px;
+    :deep(.arco-list-content) {
+      overflow-x: hidden;
+    }
+
+    :deep(.arco-card-meta-title) {
+      font-size: 14px;
+    }
+  }
+  :deep(.arco-list-col) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  :deep(.arco-list-item) {
+    width: 33%;
+  }
+
+  :deep(.block-title) {
+    margin: 0 0 12px 0;
+    font-size: 14px;
+  }
+  :deep(.list-wrap) {
+    // min-height: 140px;
+    .list-row {
+      align-items: stretch;
+      .list-col {
+        margin-bottom: 16px;
+      }
+    }
+    :deep(.arco-space) {
+      width: 100%;
+      .arco-space-item {
+        &:last-child {
+          flex: 1;
+        }
+      }
+    }
+  }
+</style>
